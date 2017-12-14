ccb8806327e44368fb8315c6de2bcc38d1bedab2
VasilyevaEkaterina <vasilyeva.ek.i@gmail.com>
2017-12-14 22:06:30 +0000

    ie: map

--- build/js/map.js
+++ build/js/map.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var contactsMap = document.getElementById('map');
+contactsMap.classList.remove('contacts__map--hidden');
+
+
 function initMap() {
   var uluru = {lat: 59.938982, lng: 30.323158};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(contactsMap, {
     zoom: 12,
     scrollwheel: true,
     center: uluru,
